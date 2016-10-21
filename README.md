3b09793aa69d244c0ce5cd7e4195574625645d6b
kvvzr <kwzr@twinkrun.net>
2016-10-21 15:48:21 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# kiosk-mode-browser
+
+## Usage
+
+```
+$ npm install
+$ npm config set kiosk-mode-browser:url http://127.0.0.1
+$ npm start
+```
---
+++ main.js
@@ -0,0 +1,29 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+let mainWindow
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({width: 800, height: 600, kiosk: true})
+
+  mainWindow.loadURL(process.env.npm_package_config_url)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
